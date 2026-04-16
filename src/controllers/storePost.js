80d789a1f86bb958751a9d17841c30e493bbc60a
Bastien <bastien-stachs@hotmail.fr>
2026-04-16 09:44:33 +0200

    Ajout de la mention de l'auteur sur l'article

--- src/controllers/storePost.js
+++ src/controllers/storePost.js
@@ -1,21 +1,26 @@
 const BlogPost = require('../models/BlogPost');
+const User = require('../models/User');
 const path = require('path');
 
 module.exports = async (req, res) => {
     const { title, body } = req.body;
-
     const image = req.files && req.files.image ? req.files.image.name : null;
 
     if (image) {
         req.files.image.mv(path.join(__dirname, '../../public/images', image), (err) => {
-            if (err) {
-                console.error("Error uploading image:", err);
-            } else {
-                console.log("Image uploaded successfully");
-            }
+            if (err) console.error("Error uploading image:", err);
         });
     }
 
-    await BlogPost.create({ title, body, image, userid: req.session.userId });
+    const user = await User.findById(req.session.userId);
+
+    await BlogPost.create({
+        title,
+        body,
+        image,
+        username: user.username,
+        userid: req.session.userId
+    });
+
     res.redirect('/');
 };
